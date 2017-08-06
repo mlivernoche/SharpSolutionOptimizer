4bc12dff7fb38a6d2c6666e8b549954ad36b4876
Matthew Livernoche <mattalivernoche@gmail.com>
2017-08-06 10:43:48 -0700

    Update SharpSolutionOptimizer.cs
    Making Mutation thread-safe (see https://stackoverflow.com/a/38530622 for solution and https://blogs.msdn.microsoft.com/pfxteam/2009/02/19/getting-random-numbers-in-a-thread-safe-way/ for more information).
    
    Added async RunParallelOptimization, which allows one to create more solutions in parallel.

--- SharpSolutionOptimizer.cs
+++ SharpSolutionOptimizer.cs
@@ -1,40 +1,87 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace SharpSolutionOptimizer
 {
+
     public abstract class Optimization<T> where T : ISolution
     {
         public delegate bool Constraint(T sol);
-        public static Random Mutation = new Random();
 
-        public IList<T> CompletedSolutions { get; } = new List<T>();
-        public IList<Constraint> Constraints { get; } = new List<Constraint>();
+        [ThreadStatic]
+        private static Random mutator;
+
+        protected static Random Mutation => mutator ?? (mutator = new Random());
+
+        public IEnumerable<T> CompletedSolutions { get; set; }
+        public IEnumerable<Constraint> Constraints { get; protected set; }
 
         public abstract T CreateSolution();
-        public abstract T GetBestSolution();
+        public abstract T GetBestSolution(IEnumerable<T> solutionlist);
 
-        public void Add(Constraint con)
+        public Optimization()
         {
-            Constraints.Add(con);
+
         }
 
-        public void Add(T sol)
+        public Optimization(IEnumerable<Constraint> constraints)
         {
-            CompletedSolutions.Add(sol);
+            Constraints = constraints;
         }
 
-        public IList<bool> ValidateSolution(T sol)
+        /// <summary>
+        /// Creates a specified amount of solutions.
+        /// </summary>
+        /// <param name="amount">The amount of solutions to create.</param>
+        /// <returns>An enumerable object of the solutions.</returns>
+        public IEnumerable<T> CreateMultipleSolutions(int amount)
         {
-            return Constraints.Select(x => x(sol)).ToList();
+            var list = new List<T>(amount);
+
+            for(int i = 0; i < amount; i++)
+            {
+                list.Add(CreateSolution());
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// Evaluates a solution subject to constraints and returns an enumerable object with the boolean value of each constraint.
+        /// </summary>
+        /// <param name="sol">The solution.</param>
+        /// <returns>An enumerable object with the boolean value of each constraint</returns>
+        public IEnumerable<bool> ValidateSolution(T sol)
+        {
+            return Constraints.Select(x => x(sol));
+        }
+
+        /// <summary>
+        /// This will execute execute Task.Run, in which the tasks is to find the best solution in an IEnumerable of possible solutions. It will then return the best of those best.
+        /// </summary>
+        /// <param name="solutionspertask">The amount of solutions each task should find.</param>
+        /// <param name="numoftasks">The amount of tasks to submit to the thread pool.</param>
+        /// <returns>The best of the best of the best, sir! With honors.</returns>
+        public async Task<T> RunParallelOptimization(int solutionspertask, int numoftasks)
+        {
+            var tasks = new Task<T>[numoftasks];
+
+            for (int i = 0; i < numoftasks; i++)
+            {
+                tasks[i] = Task.Run(() => GetBestSolution(CreateMultipleSolutions(solutionspertask)));
+            }
+
+            await Task.WhenAll(tasks);
+            return GetBestSolution(tasks.Select(x => x.Result));
         }
     }
 
     public interface ISolution
     {
         bool IsValid { get; }
-        IList<bool> Constraints { get; set; }
+        void Validate(IEnumerable<bool> constraints);
     }
 
     public interface ISolution<T> : ISolution
